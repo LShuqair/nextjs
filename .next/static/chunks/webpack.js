e65fa68961f6a7f7e46625e5d4ed71a647eb9470
Luna Shuqair <luna.shuqair89@gmail.com>
2021-10-28 10:10:17 -0400

    adding file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a56eec24ce85e985b9b";
+/******/ 	var hotCurrentHash = "a670c0dfd141489f3417";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
